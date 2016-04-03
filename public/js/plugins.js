3cd32b603cf8aa07af4264a45b4d6c3fe1601225
Liam McLeod <dual1tyx@hotmail.co.uk>
2016-04-03 17:19:42 +0100

    styling changes and mobile menu working

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,31 +1,29 @@
 $( document ).ready(function() {
-
+  function toggleMenu() {
+      $('#floating-menu').toggleClass('hide');
+  };
+  function toggleHide(id) {
+      $(id).toggleClass('hide');
+  };
+  function toggleDisplay(id) {
+      $(id).css('display:block');
+  };
     if (screen.width <= 375){
         $('#icon').click(function() {
             $(this).toggleClass('open');
+            $('#mobile-menu').toggleClass('hide');
         });
-        //TODO account for offscreen dimensions and stop it
-        $.fn.draggable = function() {
-            var offset = null;
-            var start = function(e) {
-                var orig = e.originalEvent;
-                var pos = $(this).position();
-                offset = {
-                    x: orig.changedTouches[0].pageX - pos.left
-                };
-            };
-            var moveMe = function(e) {
-                e.preventDefault();
-                var orig = e.originalEvent;
-                $(this).css({
-                    left: orig.changedTouches[0].pageX - offset.x
-                });
-            };
-            this.bind("touchstart", start);
-            this.bind("touchmove", moveMe);
-        };
-        // TODO LEFT CANNOT BE < 0 or > 375
-        $("#hamburger").draggable({ containment: "body" });
-    }
+        $('#mobile-menu').click(function() {
+            $(this).toggleClass('hide');
+            $('#icon').toggleClass('open');
+        });
+        $('#arrow').click(function() {
+            // $(this).toggleClass('open');
+            $("a[href='#body-top']").click(function() {
+            $("html, body").animate({ scrollTop: 0 }, "fast");
+            return false;
+          });
 
+        });
+    }
 });
